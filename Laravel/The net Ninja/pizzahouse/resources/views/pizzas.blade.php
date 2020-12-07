63aed371c8c9dd5292b7cc617fa872e6a8ee5841
Saqib <saqib29111@gmail.com>
2020-12-07 22:13:56 +0600

    craete pizzas blade + if in blade engine + Route for pizzas blade template

--- Laravel/The net Ninja/pizzahouse/resources/views/pizzas.blade.php
+++ Laravel/The net Ninja/pizzahouse/resources/views/pizzas.blade.php
@@ -57,38 +57,29 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+            
             <div class="content">
                 <div class="title m-b-md">
-                    Pizza House<br />
-                    The North's Best Pizzas
+                    Pizzas List
                 </div>
-                
-                <div class="links">
-                        <a href="https://laravel.com/docs">Docs</a>
-                        <a href="https://laracasts.com">Laracasts</a>
-                        <a href="https://laravel-news.com">News</a>
-                        <a href="https://blog.laravel.com">Blog</a>
-                        <a href="https://nova.laravel.com">Nova</a>
-                        <a href="https://forge.laravel.com">Forge</a>
-                        <a href="https://vapor.laravel.com">Vapor</a>
-                        <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>{{ $type }} - {{ $base }} - {{ $price }}</p>
+                @if($price > 15)
+                    <p>This pizza is expensive</p>
+                @elseif($price < 5)
+                    <p>this pizza is cheap</p>
+                @else
+                    <p>this pizza  is normlly priced</p>
+                @endif
+
+                @unless($base == 'cheesy christ')
+                    <p>Dont' have a cheesy crust</p>
+                @endunless
+
+                @php
+                    $name = 'Saqib';
+                    echo($name);
+                @endphp
             </div>
-            
         </div>
     </body>
 </html>
--- Laravel/The net Ninja/pizzahouse/routes/web.php
+++ Laravel/The net Ninja/pizzahouse/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function(){
+    $pizza = ['type' => 'hawain',
+    'base' => 'gralic christ',
+    'price' => 10
+];
+    return view('pizzas', $pizza);
+});
