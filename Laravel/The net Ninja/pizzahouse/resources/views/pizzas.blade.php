8e31fb6fd24b72406048f00587d5542b3dfae693
Saqib <saqib29111@gmail.com>
2020-12-08 01:36:29 +0600

    route parameters/wildcards

--- Laravel/The net Ninja/pizzahouse/resources/views/pizzas.blade.php
+++ Laravel/The net Ninja/pizzahouse/resources/views/pizzas.blade.php
@@ -10,15 +10,7 @@
         
             <p> {{ $name }} </p>
             <p> {{ $age }} </p>
-        
-        <!-- @for($i=0; $i < 5; $i++)
-            <p>value of i is {{ $i }}</p>
-        @endfor  -->
-
-        <!-- @for($i = 0; $i < count($pizzas); $i++)
-            <p>{{ $pizzas[$i]['type'] }}</p>
-        @endfor -->
-
+       
         @foreach($pizzas as $pizza)
             <div>
                 {{ $loop->index }} {{ $pizza['type'] }} - {{ $pizza['base'] }}
