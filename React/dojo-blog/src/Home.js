2ce34b6674f12ee982ddeee7f5f948fb100cdfa9
Saqib <saqib29111@gmail.com>
2021-01-07 00:31:01 +0600

    Outputting list

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
-    // let name = 'Saqib';
-    const [name, setName] = useState('saqib');
-    const [age, setAge] = useState(25);
 
-    const handleClick = () => {
-        setName('Aminul');
-        setAge(30);
-    }
+    const [blogs, setBlog] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome partyt', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tools', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
