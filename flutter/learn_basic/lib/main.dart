ed70ec90881c2d1df56f5e7e3e92f9c37eb056d2
Saqib <saqib29111@gmail.com>
2021-07-25 16:00:54 +0600

    Stateless->non-dynamic and Statefull->dynamic

---
+++ flutter/learn_basic/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("This is my first app"),
+            RaisedButton(onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
