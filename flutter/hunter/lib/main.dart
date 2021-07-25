6aa3bf3de96fdeba36281e79110c3d35bd75dd13
Saqib <saqib29111@gmail.com>
2021-07-25 12:55:14 +0600

    Scaffolder, appBar, floatingActionButton

--- flutter/hunter/lib/main.dart
+++ flutter/hunter/lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Text("Learn Flutter"),
+      home: Scaffold(
+
+        appBar: AppBar(
+          title: Text("Learn Flutter"),
+        ),
+
+        body: Center(
+          child: Text("Hey buddy what's going on"),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text("+")
+        ),
+
+      ),
     );
   }
 }
