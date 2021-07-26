340ae49d3df49a87ce4a9771e03831216f48d2d7
Saqib <saqib29111@gmail.com>
2021-07-26 23:12:13 +0600

    PageView with StatefulWidget

--- flutter/flutter_basic/lib/main.dart
+++ flutter/flutter_basic/lib/main.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/page1.dart';
+import 'package:flutter_basic/page2.dart';
+import 'package:flutter_basic/page3.dart';
+import 'package:flutter_basic/page4.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
-void main() {
-  runApp(MyApp());
+void main() =>  runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  PageController _controller = PageController(
+    initialPage: 3,
+  );
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          controller: _controller,
           children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Container(color: Colors.green)
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(color: Colors.blue)
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(color: Colors.yellow)
-            )
+            Page1(),
+            Page2(),
+            Page3(),
+            Page4(),
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
-
