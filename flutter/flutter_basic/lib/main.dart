0253d2117d14e7933b5e18e117ba73634ba4ece9
Saqib <saqib29111@gmail.com>
2021-07-28 00:38:49 +0600

    Bottom navigationBar Widget

--- flutter/flutter_basic/lib/main.dart
+++ flutter/flutter_basic/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/second.dart';
+import 'package:flutter_basic/five.dart';
+import 'package:flutter_basic/four.dart';
+import 'package:flutter_basic/one.dart';
+import 'package:flutter_basic/three.dart';
+import 'package:flutter_basic/two.dart';
 
 
 void main() =>  runApp(MyApp());
@@ -7,33 +11,63 @@ void main() =>  runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override  
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _currentindex = 0;
+  var pages = [
+    One(),
+    Two(),
+    Three(),
+    Four(),
+    Five(),
+  ];
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: CircleAvatar(
-          radius:50,
-          child: GestureDetector(
-            child: Hero(
-              tag: "add",
-              child: Icon(
-                Icons.add_a_photo,
-                size: 50,
-              )
-            ),
-            onTap: () {
-              Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Second())
-              );
-            },
-          )
-        )
-      )
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentindex,
+        items: [
+        BottomNavigationBarItem(
+          backgroundColor: Colors.cyan,
+          icon: Icon((Icons.message)),
+          title: Text("Message"),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon((Icons.call)),
+          title: Text("Call"),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon((Icons.pan_tool)),
+          title: Text("pan tool"),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon((Icons.radio)),
+          title: Text("Radio"),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon((Icons.contact_page)),
+          title: Text("Contact"),
+        ),
+      ],
+      onTap: (index) {
+        setState(() {
+          _currentindex=index;
+        });
+      }
+      ),
+      body: pages[_currentindex],
+      
     );
   }
 }
