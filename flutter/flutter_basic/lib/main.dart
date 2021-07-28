84a7ce308977eb0261e50ab821b53c9310c4a464
Saqib <saqib29111@gmail.com>
2021-07-28 12:16:16 +0600

    SilverAppBar

--- flutter/flutter_basic/lib/main.dart
+++ flutter/flutter_basic/lib/main.dart
@@ -5,27 +5,55 @@ import 'package:flutter_basic/two.dart';
 
 void main() =>  runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override   
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.camera)),
-                Tab(icon: Icon(Icons.message)),
-              ],
-            )
-          ),
-          body: TabBarView(children: [
-            One(),
-            Two(),
-          ]),
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              backgroundColor: Colors.red,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Easy Explanation"),
+              ),
+            ),
+              SliverList(delegate: SliverChildListDelegate(<Widget>[
+                addDetails("One", "This is one"),
+                addDetails("Two", "This is two"),
+                addDetails("Three", "This is three"),
+                addDetails("Four", "This is four"),
+                addDetails("Five", "This is five"),
+                addDetails("Six", "This is six"),
+                addDetails("Seven", "This is seven"),
+                addDetails("Eight", "This is eight"),
+                addDetails("Nine", "This is nine"),
+                addDetails("Ten", "This is ten"),
+                addDetails("Elaven", "This is elaven"),
+              ]))
+          ],
         ),
       ),
     );
   }
+}
+
+Widget addDetails(
+  String name,
+  String description,
+) {
+  return ListTile(
+    title: Text(name),
+    subtitle: Text(description),
+    leading: CircleAvatar(
+      child: Text(name[0]),
+    )
+  );
 }
